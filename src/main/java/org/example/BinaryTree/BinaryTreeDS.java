002fed0969aa7fd108ccc57bc83575639067a01b
Gabriel <prismaticnetwork12@gmail.com>
2022-11-29 14:35:44 -0300

    feat: understanding what is a BinaryTree and Trees intro

---
+++ src/main/java/org/example/BinaryTree/BinaryTreeDS.java
@@ -0,0 +1,11 @@
+package org.example.BinaryTree;
+
+public class BinaryTreeDS {
+  public static void main(String[] args) {
+    //Binary tree is basically a tree with maximun of 2 children
+    // that's why is binary 0 or 1, in that case can be both
+    //               (1)
+    //          (2)      (3)
+    //        (4) (5)  (6) (7)
+  }
+}
---
+++ src/main/java/org/example/TreeDataStructureIntro/Trees.java
@@ -0,0 +1,18 @@
+package org.example.TreeDataStructureIntro;
+
+public class Trees {
+  public static void main(String[] args) {
+    //Tree -> a non-linear data structure where nodes and edges are organized in a hierarchy
+    // a real example of this, could be a family tree
+    // abstract examples: File explorer, Databases, DNS and HTML DOM
+    // top of a tree is the root node
+    //                            root
+    //                            (A)
+    //            branch -> (B)          (C)    <- Branch
+    //         SIBLINGS: (D)  -> (E)    ->    (F)
+    //                (leaf)    (leaf)         (leaf)
+    // Size of a tree = # of nodes
+    // depth of a tree = # of edges below the root node
+    // height = # of edges above furthest leaf node
+  }
+}
